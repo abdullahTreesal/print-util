183b4c72e02d4623e1154a2baa057603b39742f9
Abdullah Saadeddin <abdullah@treesal.com>
2024-11-23 16:46:51 +0200

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
+git rm -r --cached node_modules/
+git rm --cached package-lock.json
+git rm --cached .env
+git rm --cached .DS_Store
+git commit -m "Remove specified files and folders from the repository"
 git add .
 git commit -m "test"
 git push
